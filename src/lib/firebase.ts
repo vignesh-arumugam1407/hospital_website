2ebc1d97b36f38959fc8a77deefda4e57f62e776
vignesh-arumugam1407 <vigneswe2312@gmail.com>
2026-03-29 17:30:27 +0530

    fixed firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBn2ePXc2U6VjWcNdSstZqgIjJ2FqMdcs",
+  authDomain: "laskmihospital.firebaseapp.com",
+  projectId: "laskmihospital",
+  storageBucket: "laskmihospital.firebasestorage.app",
+  messagingSenderId: "627815625185",
+  appId: "1:627815625185:web:67af1cf9a9ca2f792c62a0",
+  measurementId: "G-JV8K0PY4RP",
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
